db344aa70c7f1393289b6a96546352484ff5372a
pedrolpmd <pedro.dias@olxbr.com>
2025-02-04 15:58:31 -0300

    chore: makes database connect more robust

--- infra/database.js
+++ infra/database.js
@@ -9,19 +9,16 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD
   })
 
-  await client.connect()
-
   try {
+    await client.connect()
     const result = await client.query(queryObject)
     return result;
   } catch (err) {
     console.error(err);
+    throw err
   } finally {
     await client.end()
   }
-  
-  await client.end()
-
 }
 
 export default {
