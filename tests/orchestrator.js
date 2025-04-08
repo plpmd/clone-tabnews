701d4f8af1ead35f3650fb44879cae8abffed782
pedrolpmd <pedro.dias@olxbr.com>
2025-04-08 10:02:04 -0300

    chore: optimize orchestrator

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -12,8 +12,10 @@ async function waitForWebServer() {
 
   async function fetchStatusPage() {
     const response = await fetch('http://localhost:3000/api/v1/status')
-    const responseBody = await response.json()
 
+    if(response.status !== 200){
+      throw Error()
+    }
   }
 }
 
