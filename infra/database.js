87ec8a8814427b64c9b7352a744b1f145a60a2e8
pedrolpmd <pedro.dias@olxbr.com>
2025-04-09 09:20:25 -0300

    fix: prettier

--- infra/database.js
+++ infra/database.js
@@ -1,39 +1,39 @@
-import { Client } from 'pg'
+import { Client } from "pg";
 
 async function query(queryObject) {
   let client;
 
   try {
-    client = await getNewClient()
-    const result = await client.query(queryObject)
+    client = await getNewClient();
+    const result = await client.query(queryObject);
     return result;
   } catch (err) {
     console.error(err);
-    throw err
+    throw err;
   } finally {
-    await client.end()
+    await client.end();
   }
 }
 
-async function getNewClient(){
+async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues()
-  })
+    ssl: getSSLValues(),
+  });
 
-  await client.connect()
+  await client.connect();
   return client;
 }
 
 export default {
   query,
-  getNewClient
-}
+  getNewClient,
+};
 
 function getSSLValues() {
-  return process.env.NODE_ENV === 'production' ? true : false
+  return process.env.NODE_ENV === "production" ? true : false;
 }
