c8e2a537f7031b7ac48562c0dc46e8756b77fc9e
pedrolpmd <pedro.dias@olxbr.com>
2025-05-07 12:29:06 -0300

    fix: lint

--- pages/status/index.js
+++ pages/status/index.js
@@ -4,9 +4,8 @@ async function fetchAPI(key) {
   const response = await fetch(key);
   const responseBody = await response.json();
 
-  return responseBody
+  return responseBody;
 }
-
 
 export default function StatusPage() {
   return (
@@ -15,40 +14,40 @@ export default function StatusPage() {
       <UpdatedAt />
       <DBInfo />
     </>
-  )
+  );
 }
 
 function UpdatedAt() {
-  const { data, isLoading } = useSWR('/api/v1/status', fetchAPI, {
+  const { data, isLoading } = useSWR("/api/v1/status", fetchAPI, {
     refreshInterval: 2000,
-    dedupingInterval: 2000
-  })
+    dedupingInterval: 2000,
+  });
 
-  let updateAtText = "Carregando..."
-  if(!isLoading) {
-    updateAtText = new Date(data.updated_at).toLocaleString('pt-BR')
+  let updateAtText = "Carregando...";
+  if (!isLoading) {
+    updateAtText = new Date(data.updated_at).toLocaleString("pt-BR");
   }
 
   return (
     <>
       <div>Última atualização: {updateAtText} </div>
     </>
-  )
+  );
 }
 
 function DBInfo() {
-  const { data, isLoading } = useSWR('/api/v1/status', fetchAPI, {
+  const { data, isLoading } = useSWR("/api/v1/status", fetchAPI, {
     refreshInterval: 2000,
-    dedupingInterval: 2000
-  })
+    dedupingInterval: 2000,
+  });
 
-  let dbVersionText = "Carregando..."
-  let dbMaxConnectionsText = "Carregando..."
-  let dbOpenedConnectionText = "Carregando..."
-  if(!isLoading) {
-    dbVersionText = data.dependencies.database.version
-    dbMaxConnectionsText = data.dependencies.database.max_connections
-    dbOpenedConnectionText = data.dependencies.database.opened_connections
+  let dbVersionText = "Carregando...";
+  let dbMaxConnectionsText = "Carregando...";
+  let dbOpenedConnectionText = "Carregando...";
+  if (!isLoading) {
+    dbVersionText = data.dependencies.database.version;
+    dbMaxConnectionsText = data.dependencies.database.max_connections;
+    dbOpenedConnectionText = data.dependencies.database.opened_connections;
   }
 
   return (
@@ -57,5 +56,5 @@ function DBInfo() {
       <div>Máximo de conexões do banco de dados: {dbMaxConnectionsText} </div>
       <div>Conexões abertas no banco de dados: {dbOpenedConnectionText} </div>
     </>
-  )
+  );
 }
