d5e7a2807aa83dc5b3057f3bcb3ee658dcc8b4e3
plpmd <70477382+plpmd@users.noreply.github.com>
2025-05-27 16:06:47 -0300

    feat: add initial implementation of status page (#33)
    * feat: status page
    
    * fix: lint
    
    * refactor: use dbStatusInformation
    
    * fix: lint
    
    ---------
    
    Co-authored-by: pedrolpmd <pedro.dias@olxbr.com>

---
+++ pages/status/index.js
@@ -0,0 +1,70 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+
+  return responseBody;
+}
+
+export default function StatusPage() {
+  return (
+    <>
+      <h1>Status</h1>
+      <UpdatedAt />
+      <DBInfo />
+    </>
+  );
+}
+
+function UpdatedAt() {
+  const { data, isLoading } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+    dedupingInterval: 2000,
+  });
+
+  let updateAtText = "Carregando...";
+  if (!isLoading) {
+    updateAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
+
+  return (
+    <>
+      <div>Última atualização: {updateAtText} </div>
+    </>
+  );
+}
+
+function DBInfo() {
+  const { data, isLoading } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+    dedupingInterval: 2000,
+  });
+
+  let dbStatusInformation = "Carregando...";
+
+  if (!isLoading && data) {
+    dbStatusInformation = (
+      <>
+        <div>
+          Versão do banco de dados: {data.dependencies.database.version}{" "}
+        </div>
+        <div>
+          Máximo de conexões do banco de dados:{" "}
+          {data.dependencies.database.max_connections}{" "}
+        </div>
+        <div>
+          Conexões abertas no banco de dados:{" "}
+          {data.dependencies.database.opened_connections}{" "}
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h2>Database</h2>
+      {dbStatusInformation}
+    </>
+  );
+}
