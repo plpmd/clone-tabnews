81943e6ab1890fb8dc1d624149def8e78c236985
Pedro Dias <144837055+pedrolpmd@users.noreply.github.com>
2025-08-06 08:27:23 -0300

    feat: add migrator model (#36)
    * feat: add migrator model
    
    * chore: add lint-staged

---
+++ models/migrator.js
@@ -0,0 +1,52 @@
+import { resolve } from "node:path";
+import migrationRunner from "node-pg-migrate";
+import database from "infra/database";
+
+const defaultMigrationsOption = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationsOption,
+      dbClient,
+    });
+
+    return pendingMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOption,
+      dbClient,
+      dryRun: false,
+    });
+
+    return migratedMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  runPendingMigrations,
+  listPendingMigrations,
+};
+
+export default migrator;
