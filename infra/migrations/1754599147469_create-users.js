1ea9cd7e547608521f09561386b1db20d29da0a7
pedrolpmd <pedro.dias@olxbr.com>
2025-09-08 10:29:13 -0300

    fix: typo on users migration

--- infra/migrations/1754599147469_create-users.js
+++ infra/migrations/1754599147469_create-users.js
@@ -21,12 +21,12 @@ exports.up = (pgm) => {
     },
     created_at: {
       type: "timestamptz",
-      default: pgm.func("timezone('utc', now()"),
+      default: pgm.func("timezone('utc', now())"),
       notNull: true,
     },
     updated_at: {
       type: "timestamptz",
-      default: pgm.func("timezone('utc', now()"),
+      default: pgm.func("timezone('utc', now())"),
       notNull: true,
     },
   });
