68f14ff0199ff5aaeeacd4984fb9d88b77974823
plpmd <pedroluanpd@gmail.com>
2026-03-04 17:23:58 -0300

    fix: add no cache control in get user route

--- pages/api/v1/user/index.js
+++ pages/api/v1/user/index.js
@@ -13,9 +13,13 @@ async function getHandler(request, response) {
 
   const sessionObject = await session.findOneValidByToken(sessionToken);
   const renewedSessionObject = await session.renew(sessionObject.id);
-  const userFound = await user.findOneById(sessionObject.user_id);
-
   controller.setSessionCookie(renewedSessionObject.token, response);
 
+  const userFound = await user.findOneById(sessionObject.user_id);
+
+  response.setHeader(
+    "Cache-Control",
+    "no-store, no-cache, max-age=0, must-revalidate",
+  );
   return response.status(200).json(userFound);
 }
